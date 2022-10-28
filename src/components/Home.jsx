be65d5ef18276fe7dc79c7e9c0f75efb1ffeea9d
Whalay <soliuy5@gmail.com>
2022-10-28 09:02:30 +0100

    Edited the links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,66 +13,83 @@ const Home = () => {
       <div className="text-md text-center h-screen">
         <div className="my-10 relative">
           <span className="absolute top-3 right-8 border rounded-full p-2 py-3 md:hidden">
-            {" "}
             <img src={more} alt="" />
           </span>
           <span className="absolute top-3 right-64 border rounded-full p-2 py-3 hidden md:block">
             <img src={share} alt="" />
           </span>
-          <img src={profile} alt="" className="m-auto" id="profile_img" />
+          <img src={profile} alt="" className="m-auto" id="profile__img" />
           <p className="font-bold text-3xl">Soliu Yusuf</p>
         </div>
-        <ul className="text-center">
-          <a href="http://twitter.com/holar102" target="_blank" id="twitter">
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]">
-              Twitter Link
-            </li>
+        <div className="text-center flex flex-col">
+          <a
+            href="http://twitter.com/holar102"
+            target="_blank"
+            id="twitter"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
+          >
+            Twitter Link
           </a>
 
-          <li id="slack"className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD] hidden">
+          <a
+            href="/"
+            id="slack"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD] hidden"
+          >
             Holar
-          </li>
-
-          <a href="http://books.zuri.team" target="_blank" id="btn_zuri">
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]">
-              Zuri Team
-            </li>
           </a>
-          <a href="http://books.zuri.team" target="_blank" i="books">
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]">
-              Zuri Books
-            </li>
+
+          <a
+            href="http://books.zuri.team"
+            target="_blank"
+            id="btn__zuri"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
+          >
+            Zuri Team
+          </a>
+          <a
+            href="http://books.zuri.team"
+            target="_blank"
+            id="books"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
+          >
+            Zuri Books
           </a>
           <a
             href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>"
             target="_blank"
-            id="book_python"
+            id="book__python"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
           >
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]">
-              Python Books
-            </li>
+            Python Books
           </a>
-          <a href="https://background.zuri.team" target="_blank" id="pitch">
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]">
-              Background Check for Coders
-            </li>
+          <a
+            href="https://background.zuri.team"
+            target="_blank"
+            id="pitch"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
+          >
+            Background Check for Coders
           </a>
           <a
             href="https://books.zuri.team/design-rules"
             target="_blank"
-            id="book_design"
+            id="book__design"
+            className="bg-[#EAECF0] p-4 m-4 md:mx-20 hover:bg-[#D0D5DD] active:bg-[#D0D5DD]"
           >
-            <li className="bg-[#EAECF0] p-4 m-4 md:mx-20 active:bg-[#D0D5DD] hover:bg-[#D0D5DD]">
-              Design Books
-            </li>
+            Design Books
           </a>
-        </ul>
+        </div>
         <div className="flex justify-center gap-5">
-        <a href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U0487V7BULV"><img src={slack} alt="" /></a>
-       <a href="https://github.com/whalay"><img src={github} alt="" /></a>
+          <a href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U0487V7BULV">
+            <img src={slack} alt="" />
+          </a>
+          <a href="https://github.com/whalay">
+            <img src={github} alt="" />
+          </a>
         </div>
       </div>
-      <footer className="test-left p-5 mt-36 ">
+      <footer className="test-left p-5 mt-28 ">
         <hr />
         <div className="p-5  md:flex justify-between">
           <img src={I4G} alt="" />
